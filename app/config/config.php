bcf444feba98512c352009c01cd4100ddf81d05f
Abel Olayinka <56263750+maescode@users.noreply.github.com>
2021-01-10 22:32:51 +0100

    Add files via upload
    add a social network php app built on the MVC framework

---
+++ app/config/config.php
@@ -0,0 +1,14 @@
+<?php
+  // DB Params
+  define("DB_HOST", "localhost");
+  define("DB_USER", "root");
+  define("DB_PASS", "");
+  define("DB_NAME", "mvc");
+
+  // App Root
+  define('APPROOT', dirname(dirname(__FILE__)));
+  // URL Root
+  define('URLROOT', 'http://localhost/mvc-shareposts');
+  // Site Name
+  define('SITENAME', 'Maescode SharePosts');
+  
---
+++ readMe.txt
@@ -0,0 +1,2 @@
+1. Change the file path in public folder .htaccess to your project path.
+2. Modify config/config.php to suite your specification.
